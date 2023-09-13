e28140d0df2d1c90b2c989a0b80e88791c2fc54d
grootgidguard06 <vietchuong865@gmail.com>
2023-09-14 00:01:02 +0700

    first commit

---
+++ tinh_tong_hieu_2_so_nguyen.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int soThu1, soThu2;
+    cin >> soThu1;
+    cin >> soThu2;
+    int tong, hieu;
+    tong = soThu1 + soThu2;
+    hieu = soThu1 - soThu2;
+    cout << "tong: " << tong << endl;
+    cout << "hieu: " << hieu << endl;
+    return 0;
+}
